fbd6813b8eb119e13013f17df50133cf6a3cdff8
dibyatanoy <dibyatanoy.bhattacharjee@yale.edu>
2017-01-01 20:59:21 +0600

    adding "show reminders" feature

--- index.js
+++ index.js
@@ -494,6 +494,7 @@ const actions = {
             // Yay, we found our recipient!
             // Let's forward our bot response to her.
             // We return a promise to let our bot know when we're done sending
+            console.log('show' in context)
             return sendTextMessage(recipientId, text, context)
             // .then(() => null)
             // .catch((err) => {
